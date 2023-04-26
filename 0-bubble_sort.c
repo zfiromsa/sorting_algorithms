e4b44c58ac8f6895090a9ecfa9c75acf938545b5
zfiromsa <mdzm941@gmail.com>
2023-04-25 19:19:58 -0700

    go

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,22 +11,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        /** Last i elements are already in place **/
-        for (j = 0; j < size - i - 1; j++)
-        {
-            /* If the current element is greater than the next element, swap them */
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-	print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		/** Last i elements are already in place **/
+		for (j = 0; j < size - i - 1; j++)
+		{
+			/* If the current element is greater than the next element, swap them */
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+		print_array(array, size);
+	}
 }
+
