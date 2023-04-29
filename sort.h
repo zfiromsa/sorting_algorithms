b939839298d684fa4b8eff0c74c694a3cabf7656
zfiromsa <mdzm841@gmail.com>
2023-04-29 00:02:38 -0700

    go

--- sort.h
+++ sort.h
@@ -13,7 +13,7 @@ typedef struct listint_s
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
-}listint;
+}listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
