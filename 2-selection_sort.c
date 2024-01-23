d5d408e39baaef196043a4eabee4d07faf5d1957
zfiromsa <mdzm841@gmail.com>
2024-01-23 04:16:17 -0800

    go

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * election_sort -  sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: array of unsorted list.
+ * @size: the size of array.
+ * Return: nothing.
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, _min;
+	int tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		_min = i;
+		for ( j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[_min])
+				_min = j;
+		}
+		if (_min != i)
+		{
+			tmp = array[_min];
+			array[_min] = array[i];
+			array[i] = tmp;
+		}
+		print_array(array, size);
+		i++;
+	}
+}
+
