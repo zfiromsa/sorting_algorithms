fb6b7cc7a7fe50cfc137d2e7980d484a76c0803e
zfiromsa <mdzm841@gmail.com>
2024-01-27 02:09:55 -0800

    go

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,10 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, tmp;
 
+	if (!array)
+	{
+		fprintf(stderr, "Anything");
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
