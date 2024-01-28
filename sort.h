4070775d752f071913296ea697fb140a56044e87
zfiromsa <mdzm841@gmail.com>
2024-01-27 17:23:13 -0800

    go

--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 #include <string.h>
 
 void print_list(const listint_t *list);
+void counting_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
